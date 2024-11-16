300458ac5587045ea2a4dd40e92560df84eeadd4
zhuguangwen <zhuguangwen@zealshow.com>
2024-11-16 23:35:53 +0800

    namespace

--- examples/FixedMathExample.cpp
+++ examples/FixedMathExample.cpp
@@ -5,7 +5,7 @@ using namespace Fixed64;
 
 int main() {
     // 定义一些角度（使用弧度）
-    Fixed64 angleInRadians = FixedMath::Deg2Rad * Fixed64::parseLong(45); // 将45度转换为弧度
+    Fixed64 angleInRadians = FixedMath::Deg2Rad * Fixed64(45); // 将45度转换为弧度
 
     // 计算并输出sin, cos, tan的值
     Fixed64 sinValue = FixedMath::Sin(angleInRadians);
@@ -26,8 +26,8 @@ int main() {
 
     // 使用LerpAngle来进行角度插值
     Fixed64 fromAngle = Fixed64Const::Zero; // 0度
-    Fixed64 toAngle = FixedMath::Deg2Rad * Fixed64::parseLong(90); // 90度
-    Fixed64 t = Fixed64::parseLong(0.5); // 插值参数，0.5表示中间值
+    Fixed64 toAngle = FixedMath::Deg2Rad * Fixed64(90); // 90度
+    Fixed64 t = Fixed64(0.5); // 插值参数，0.5表示中间值
 
     Fixed64 lerpAngle = FixedMath::LerpAngle(fromAngle, toAngle, t) * FixedMath::Rad2Deg;
     std::cout << "LerpAngle from 0 to 90 degrees at t=0.5: " << lerpAngle << std::endl;
