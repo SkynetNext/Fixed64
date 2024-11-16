aa6a9f2218ebb2827eed688901a6e74db8f6fd3f
zhuguangwen <zhuguangwen@zealshow.com>
2024-11-16 23:27:38 +0800

    FixedConst

--- examples/FixedMathExample.cpp
+++ examples/FixedMathExample.cpp
@@ -15,15 +15,15 @@ int main() {
     std::cout << "Tan(45 degrees): " << tanValue << std::endl;
 
     // 使用atan2来计算一个角度，并将结果转换回度
-    Fixed64 y = Fixed64::One;
-    Fixed64 x = Fixed64::One;
+    Fixed64 y = Fixed64Const::One;
+    Fixed64 x = Fixed64Const::One;
     Fixed64 atan2Value = FixedMath::Atan2(y, x);
     Fixed64 angleInDegrees = atan2Value * FixedMath::Rad2Deg;
 
     std::cout << "Atan2(1,1) in degrees: " << angleInDegrees << std::endl;
 
     // 使用LerpAngle来进行角度插值
-    Fixed64 fromAngle = Fixed64::Zero; // 0度
+    Fixed64 fromAngle = Fixed64Const::Zero; // 0度
     Fixed64 toAngle = FixedMath::Deg2Rad * Fixed64::parseLong(90); // 90度
     Fixed64 t = Fixed64::parseLong(0.5); // 插值参数，0.5表示中间值
 
